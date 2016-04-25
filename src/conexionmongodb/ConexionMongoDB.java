aa37d4a879bbcd8753234de6454f93b74df691f3
ebaldizon <emanuel.baldizon@gmail.com>
2016-04-25 13:28:59 -0600

    Get all

---
+++ src/conexionmongodb/ConexionMongoDB.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionmongodb;
+
+/**
+ *
+ * @author Emanuel
+ */
+public class ConexionMongoDB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmPersona fPersona = new frmPersona();
+        fPersona.setVisible( true );
+    }
+    
+}
